68c3db0238d34ef4743fca4cf691bbc3f4a2350f
Jhonatan Martinez <consejerojhon@gmail.com>
2024-03-05 22:02:23 -0400

    Added a User Form and Styles Files.

---
+++ public/js/form.js
@@ -0,0 +1,18 @@
+/* alert('Curso de Javascript'); */
+
+// COMMENT
+
+const val1 = 100;
+const val2 = 10;
+
+let operation = val1 + val2;
+
+let resultDescription = 'The result is ' + operation;
+
+console.log(resultDescription)
+
+function sum(val1, val2){
+    alert(val1 + val2);
+}
+
+sum(52, 12)
---
+++ public/js/index.js
@@ -0,0 +1,10 @@
+/* alert('Curso de Javascript'); */
+
+// COMMENT
+
+const username = 'jhon_mart';
+
+let description = 'The username is ' + username;
+
+console.log('Result: ', description)
+
